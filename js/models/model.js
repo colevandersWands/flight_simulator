dccd5280055e8d6dc103e7dc9e626c35d5c2cfaa
Radovan Delic <infamouszgbgd@yahoo.com>
2017-09-21 16:41:45 +0200

    folders

---
+++ js/models/model.js
@@ -0,0 +1,10 @@
+model =  {
+    fuel: 10000000000000,
+    altitude: 0,
+    get_fuel: function (){
+        return this.fuel;
+    },
+    get_altutude: function() {
+        return this.fuel;
+    }
+};
